c9b7e850bbb79f36f787f1892faa37d09cfd516f
Gemma Fernandez <gemmazuera@gmail.com>
2026-03-22 17:15:53 +0100

    Testing de base de datos

--- src/main/java/com/proyectoaa/dao/Database.java
+++ src/main/java/com/proyectoaa/dao/Database.java
@@ -1,33 +1,39 @@
 package com.proyectoaa.dao;
 
-
-import lombok.Getter;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
 
 
 public class Database {
-    @Getter
 
-    private static Connection connection;
-    public static void connect() throws ClassNotFoundException, SQLException {
-        if (connection == null) {
+    private static Jdbi jdbi;
+
+    public static void connect() {
+        if (jdbi == null) {
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/campervan_rentals", "root", "1234");
-                System.out.println("Conexión exitosa a la base de datos.");
-            }catch (Exception e){
+
+                String url = "jdbc:mariadb://localhost:3306/campervan_rentals";
+                String usuario = "root";
+                String contrasena = "1234";
+
+                jdbi = Jdbi.create(url, usuario, contrasena);
+                jdbi.installPlugin(new SqlObjectPlugin());
+
+                System.out.println("Conexión exitosa a la base de datos con JDBI.");
+
+            } catch (Exception e) {
                 System.out.println("Error al conectar: " + e.getMessage());
                 e.printStackTrace();
             }
         }
     }
 
-    public static void close() throws SQLException{
-        connection.close();
-
+    public static Jdbi getJdbi() {
+        if (jdbi == null) {
+            connect();
+        }
+        return jdbi;
     }
 }
--- src/main/java/com/proyectoaa/dao/UserDao.java
+++ src/main/java/com/proyectoaa/dao/UserDao.java
@@ -1,23 +1,20 @@
 package com.proyectoaa.dao;
 
 import  com.proyectoaa.model.User;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
-public class UserDao {
+public interface UserDao {
 
-    public void add(){
+    //Método para DAR DE ALTA
+    @SqlUpdate("INSERT INTO usuarios (nombre, email, password, rol, saldo_monedero, fecha_registro, cuenta_activa) VALUES (:nombre, :email, :password, :rol, :saldoMonedero, :fechaRegistro, :cuentaActiva)")
+    boolean registrarUsuario(@BindBean User user);
 
-    }
-
-    public void remove(){
-
-    }
-
-    public void getAll(){
-
-    }
-
-    public void update(){
-
-    }
-
+    //Método para el LOGIN
+    @SqlQuery("SELECT * FROM usuarios WHERE email = :email")
+    @RegisterBeanMapper(User.class)
+    User obtenerUsuarioPorEmail(@Bind("email") String email);
 }
