f02443b5d8d72522fbd523f0bc3f5911496e29fc
Gemma Fernandez <gemmazuera@gmail.com>
2026-03-22 18:08:18 +0100

    dao hechos

--- src/main/java/com/proyectoaa/dao/ReservationDao.java
+++ src/main/java/com/proyectoaa/dao/ReservationDao.java
@@ -1,4 +1,21 @@
 package com.proyectoaa.dao;
 
-public class ReservationDao {
+import com.proyectoaa.model.Reservation;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+public interface ReservationDao {
+
+    // Crear una nueva reserva
+    @SqlUpdate("INSERT INTO reservas (id_usuario, id_vehiculo, observaciones_cliente, dias_alquiler, coste_total, fecha_inicio_viaje, pagada_por_completo) VALUES (:idUsuario, :idVehiculo, :observacionesCliente, :diasAlquiler, :costeTotal, :fechaInicioViaje, :pagadaPorCompleto)")
+    boolean crearReserva(@BindBean Reservation reservation);
+
+    // Listar todas las reservas
+    @SqlQuery("SELECT * FROM reservas")
+    @RegisterBeanMapper(Reservation.class)
+    List<Reservation> listarTodas();
 }
--- src/main/java/com/proyectoaa/dao/VehicleDao.java
+++ src/main/java/com/proyectoaa/dao/VehicleDao.java
@@ -1,4 +1,31 @@
 package com.proyectoaa.dao;
 
-public class VehicleDao {
+import com.proyectoaa.model.Vehicle;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+public interface VehicleDao {
+
+    // Dar de alta un vehículo
+    @SqlUpdate("INSERT INTO vehiculos (modelo, matricula, imagen_url, capacidad_pasajeros, precio_por_dia, fecha_alta_flota, disponible) VALUES (:modelo, :matricula, :imagenUrl, :capacidadPasajeros, :precioPorDia, :fechaAltaFlota, :disponible)")
+    boolean registrarVehiculo(@BindBean Vehicle vehicle);
+
+    // Listado completo para el catálogo
+    @SqlQuery("SELECT * FROM vehiculos")
+    @RegisterBeanMapper(Vehicle.class)
+    List<Vehicle> listarTodos();
+
+    // Buscar uno solo por su ID
+    @SqlQuery("SELECT * FROM vehiculos WHERE id_vehiculo = :id")
+    @RegisterBeanMapper(Vehicle.class)
+    Vehicle obtenerPorId(@Bind("id") int id);
+
+    // Dar de baja
+    @SqlUpdate("UPDATE vehiculos SET disponible = false WHERE id_vehiculo = :id")
+    boolean darDeBaja(@Bind("id") int id);
 }
